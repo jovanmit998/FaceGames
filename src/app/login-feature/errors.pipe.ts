6f0017b399469f2e2ee8ee444bbaf2a6817ccc82
Jovan Mitrovic <79802504+jovanmit998@users.noreply.github.com>
2023-03-07 23:47:14 +0100

    forms styles and functionality added (#2)

---
+++ src/app/login-feature/errors.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
+
+const errorList = {
+  required: 'This field is required',
+  minlength: (minLength: number) => `Min length is ${minLength}`,
+  pattern:
+    'Password must have one uppercase character and one numeric character',
+};
+
+@Pipe({
+  name: 'error',
+  standalone: true,
+})
+export class ErrorPipe implements PipeTransform {
+  transform(value: ValidationErrors) {
+    const error = value ? Object.keys(value)[0] : null;
+    if (error === 'minlength') {
+      return errorList[error](value['minlength']?.requiredLength);
+    }
+    return error ? errorList[error] : null;
+  }
+}
